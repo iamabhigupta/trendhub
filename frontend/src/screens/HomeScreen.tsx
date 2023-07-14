25e123ccfbb5669fbde53ffa2ad031e368e88cb2
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-14 18:12:53 +0530

    changes

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -21,7 +21,7 @@ const HomeScreen = () => {
       <section className="">
         <div className="container px-5 pb-20 mx-auto">
           <h2 className="text-4xl text-gray-800 py-8 font-semibold tracking-tighter">
-            Latest Products
+            Latest Product
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {products?.map((product) => (
