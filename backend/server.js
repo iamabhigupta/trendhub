f3e1078ac893c164931287d57783c59ab67ac079
Abhishek Gupta <91587690+iamabhigupta@users.noreply.github.com>
2023-07-20 00:47:37 +0530

    Order routes and controller functions

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-dotenv.config();
+import dotenv from 'dotenv';
+import express from 'express';
+
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
 
 const port = process.env.PORT || 5000;
 
@@ -19,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
